364dc232cee771506bf1dd08c5436b266650015c
awenzhang <aweneagle@gmail.com>
2018-07-27 20:14:49 +0800

    Create Model.php

---
+++ phpmock/nospace/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: aiwenzhang
+ * Date: 2018/7/26
+ * Time: 11:32
+ */
+
+class Model
+{
+    public function getTime() {
+        return myfunc() + time();
+    }
+}
