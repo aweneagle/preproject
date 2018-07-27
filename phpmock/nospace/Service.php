e196779d6ca4da6a9a400f9966f149c9495247a0
awenzhang <aweneagle@gmail.com>
2018-07-27 20:15:14 +0800

    Create Service.php

---
+++ phpmock/nospace/Service.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: aiwenzhang
+ * Date: 2018/7/26
+ * Time: 11:24
+ */
+
+class Service
+{
+    public function getTime(Model $b) {
+        return $b->getTime();
+    }
+
+    public function getNewTime() {
+        $b = new Model();
+        return $b->getTime();
+    }
+}
